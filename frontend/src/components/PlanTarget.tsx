98a1f5b7ac316738fc7b4a7168f246b29575dca7
federico tropiane <fedetrop23@gmail.com>
2025-09-07 20:05:39 -0300

    Anadimos seccion de planes mobile

--- frontend/src/components/PlanTarget.tsx
+++ frontend/src/components/PlanTarget.tsx
@@ -1,12 +1,13 @@
-interface Props {
+export interface targetData {
     id: number,
     title: string,
     description: string,
+    image?: string
 }
-export const PlanTarget = ({id, title, description}: Props) => {
+export const PlanTarget = ({id, title, description, image}: targetData) => {
     return (
         <>
-            <div key={id} className="planTarget">
+            <div key={id} className="planTarget" style={{backgroundImage: `url(${image})`}}>
                <h2>{title}</h2>
                <p>{description}</p>
             </div>
--- frontend/src/components/TargetContainer.tsx
+++ frontend/src/components/TargetContainer.tsx
@@ -1,21 +1,26 @@
+import type { targetData } from "./PlanTarget"
+
 import TargetCarousel from "./hooks/TargetCarousel"
 import { PlanTarget } from "./PlanTarget"
 
-const planes = [
+const planes: targetData[] = [
     {
         id: 1,
         title: "Plan 1",
         description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.",
+        image: "/src/assets/white-plan.png"
     },
     {
         id: 2,
         title: "Plan 2",
         description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.",
+        image: "/src/assets/green-plan.png"
     },
     {
         id: 3,
         title: "Plan 3",
         description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.",
+        image: "/src/assets/orange-plan.png"
     },
 ]
 
@@ -26,7 +31,7 @@ export const TargetContainer = () => {
             <TargetCarousel>
                 {
                     planes.map((plan)=>(
-                        plan && <PlanTarget key={plan.id} id={plan.id} title={plan.title} description={plan.description} />
+                        plan && <PlanTarget key={plan.id} id={plan.id} title={plan.title} description={plan.description} image={plan.image}/>
                     ))
                 }
             </TargetCarousel>

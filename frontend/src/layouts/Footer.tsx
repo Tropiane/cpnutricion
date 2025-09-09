e20901ab749fcb3e7870132dedc13dc776a97eb6
federico tropiane <fedetrop23@gmail.com>
2025-09-08 21:05:15 -0300

    Anadimos iconos de apps y estilos en fuentes

--- frontend/src/layouts/Footer.tsx
+++ frontend/src/layouts/Footer.tsx
@@ -1,3 +1,7 @@
+import { TfiInstagram } from "react-icons/tfi";
+import { IoLogoWhatsapp } from "react-icons/io5";
+
+
 import { NavBar } from "../components/NavBar"
 
 export const Footer = () =>{
@@ -5,6 +9,11 @@ export const Footer = () =>{
         <footer>
             <h3 className="nunito-bold-italic">Montevideo - Uruguay</h3>
             <NavBar />
+
+            <div className="socialMedia">
+                <TfiInstagram><a href="https://www.instagram.com/cpnutricion/"></a></TfiInstagram>
+                <IoLogoWhatsapp><a href="https://wa.me/099999999"></a></IoLogoWhatsapp>
+            </div>
         </footer>
     )
 }

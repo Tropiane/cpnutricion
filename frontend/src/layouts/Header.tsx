d445fa93e172ceae6fdee6bd5cbc8e3c092f53b4
federico tropiane <fedetrop23@gmail.com>
2025-09-12 10:18:17 -0300

    diseno desktop finalizado

--- frontend/src/layouts/Header.tsx
+++ frontend/src/layouts/Header.tsx
@@ -8,7 +8,7 @@ export const Header = () =>{
             <BgVideo />
             <NavBar />
             <img src="https://drive.google.com/thumbnail?id=1W5rMPo8n299qRZuR78PQxz_dw8Hpiprv&sz=w1000" alt="Image" className="logo" />
-            <h1 className="nunito-bold-italic headerTitle">Comer bien, sentirte bien, vivir en equilibrio.</h1>
+            <h1 className="nunito-bold-italic headerTitle">Comer bien, <span>sentirte</span> bien, vivir <span>en equilibrio</span>.</h1>
         </header>
     )
 }

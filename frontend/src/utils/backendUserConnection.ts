7a7b5c5057d786c3eeeaf061dc2aad45fab7f785
federico tropiane <fedetrop23@gmail.com>
2025-12-20 09:21:47 -0300

    Modificamos endpoints backend

--- frontend/src/utils/backendUserConnection.ts
+++ frontend/src/utils/backendUserConnection.ts
@@ -7,7 +7,7 @@ interface User{
 
 const login = async (data: User)=>{
     try {
-        const res = await connection.post<{ status: number; message: string; data: object }>('/api/user/login', data);
+        const res = await connection.post<{ status: number; message: string; data: object }>('user/login', data);
 
         if(!res) return;
         

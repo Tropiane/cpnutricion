7a440c62e017f80c72ebb8dc3c8e809d99f9be3f
federico tropiane <fedetrop23@gmail.com>
2025-12-19 23:43:18 -0300

    first deploy

--- backend/src/modules/form/form.controller.ts
+++ backend/src/modules/form/form.controller.ts
@@ -26,9 +26,11 @@ class formController {
         }
     }
 
-    async deleteForm(req: Request<{},{}, {id: number}>, res: Response) {
+    async deleteForm(req: Request<{ id: number },{}, {}>, res: Response) {
         try {
-            const formID = req.body.id
+            const formID = req.params.id;
+            console.log(formID);
+            
             return (await this.service.deleteForm(formID), res.status(200));
         } catch (error) {
             console.log(error);
--- backend/src/modules/form/form.router.ts
+++ backend/src/modules/form/form.router.ts
@@ -8,7 +8,7 @@ const controller = new formController();
 router.get("/", validateAdmin, async (req: Request, res: Response) => {await controller.getForms(req, res)});
 router.post("/", async (req: Request, res: Response)=>{controller.createForm(req, res);});
 router.patch("/", async (req:Request<{},{}, {id: number, comment: string}>, res:Response)=>{await controller.updateForm(req, res)  });
-router.delete('/', async (req: Request<{},{},{id: number}>, res:Response)=>{ await controller.deleteForm(req, res)})
+router.delete('/:id', async (req: Request<{id: number},{},{}>, res:Response)=>{ await controller.deleteForm(req, res)})
 router.patch("/change-status", async (req:Request<{},{},{formId:number, status:string}>, res:Response)=>{await controller.changeFormStatus(req,res)})
 const formRouter = router;
 export default formRouter;
